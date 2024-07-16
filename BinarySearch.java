04f3f95a449e43f93704283d5ebaa857ce6e11d7
Sumit Verma <info.sumitv@gmail.com>
2024-07-16 15:45:22 +0530

    Added Order Agnostic binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,24 +3,30 @@ public class BinarySearch {
     public static void main(String[] args)
     {
         int[] arr= {1,2,3,5,7,8,9,55,121,12,20};
-        int target = 121;
-        int result = BinarySearch(arr, target);
+        int[] arr2 ={10,8,6,4,2,1};
+        int target = 6;
+        int result = BinarySearch(arr2, target);
         System.out.println(result);
     }
     public static int BinarySearch(int[] arr,int target)
     {
         int start=0;
         int end = arr.length-1;
+        boolean isAsc = arr[start]<arr[end];
         while(start<=end)
         {
-            int mid= start+(end-start)/2;
-            if(target<arr[mid])
+            int mid=start+(end-start)/2;
+            if(target == arr[mid]) return mid;
+            if (isAsc)
             {
-                end=mid+1;
-            } else if (target > arr[mid]) {
-                start=mid+1;
+              if(target<arr[mid]) end=mid+1; 
+              else start=mid+1;
             }
-            else return mid;
+            else
+            {
+            if(target>arr[mid]) end=mid+1; 
+              else start=mid+1;
+            }
         } return -1;
     }
 }
